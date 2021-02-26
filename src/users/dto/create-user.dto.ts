2ed47009a64ad0382e8779b98174ddb60ee50695
Matheus <matheus>
2021-02-26 17:48:49 -0300

    test: add class validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsPhoneNumber, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsEmail()
   @ApiProperty()
   email: string;
 
+  @IsPhoneNumber('BR')
   @ApiProperty()
   phone: string;
 
+  @MinLength(4)
   @ApiProperty()
   name: string;
 
+  @MinLength(6)
   @ApiProperty()
   password: string;
 }
