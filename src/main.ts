867c584e532e8c19461ae96409b1b09a94af1a8c
Matheus <matheus>
2021-03-03 20:04:10 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Api example')
     .setDescription('The API description')
@@ -16,6 +22,5 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
-
 }
 bootstrap();
